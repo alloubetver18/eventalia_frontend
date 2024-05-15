14644dba1a0da398608c9eb7c990028c3014fcfb
Alban Loubet Vergara <alban.loubet@controlnet.local>
2024-05-15 14:51:12 +0200

    commit05152024-sec

--- src/app/pages/datos-evento/datos-evento.component.ts
+++ src/app/pages/datos-evento/datos-evento.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+interface genero{
+  id: number,
+  denominacion: string
+}
+
+interface event{
+  id: number,
+  img: string,
+  nombre: string,
+  organizador: string,
+  fecha_hora: string,
+  ciudad: string,
+  descripcion: string
+  generos: genero[]
+}
+
 
 @Component({
   selector: 'app-datos-evento',
@@ -10,4 +27,91 @@ import { RouterLink } from '@angular/router';
 })
 export class DatosEventoComponent {
 
+  eventos: event[] = [
+    {
+      id: 1,
+      img: '../../../assets/img/feria-caballo-jerez_red.jpg',
+      nombre: 'Feria del Caballo Jerez 2024',
+      organizador: 'Ayuntamiento de Jerez',
+      fecha_hora: '30/04/2024, 15:30',
+      ciudad: 'Jerez de la Frontera, Cádiz',
+      descripcion: 'Celebra la Feria del Caballo este fin de semana en Jerez de la Frontera',
+      generos: [
+        {
+          id: 4,
+          denominacion: 'Cultura'
+        }
+      ]
+    },
+    {
+      id: 2,
+      img: '../../../assets/img/salon-manga-chiclana.jpg',
+      nombre: 'Salón del Manga Chiclana 2024',
+      organizador: 'Ayuntamiento de Chiclana de la Frontera',
+      fecha_hora: '30/04/2024, 15:30',
+      ciudad: 'Chiclana de la Frontera, Cádiz',
+      descripcion: 'Celebra el Salón del Manga de Chiclana este fin de semana en Chiclana',
+      generos: [
+        {
+          id: 4,
+          denominacion: 'Cultura'
+        }
+      ]
+    },
+    {
+      id: 3,
+      img: '../../../assets/img/torneo-ajedrez.jpg',
+      nombre: 'Torneo de Ajedrez Verano 2024',
+      organizador: 'Delegación de Cultura Provincia de Cádiz',
+      fecha_hora: '30/04/2024, 15:30',
+      ciudad: 'Cádiz, Cádiz',
+      descripcion: 'Celebra el Salón del Manga de Chiclana este fin de semana en Chiclana',
+      generos: [
+        {
+          id: 4,
+          denominacion: 'Cultura'
+        }
+      ]
+    },
+    {
+      id: 4,
+      img: '../../../assets/img/laser-tag.jpg',
+      nombre: 'Torneo de Laser Tag',
+      organizador: 'Casa de la Juventud Chiclana',
+      fecha_hora: '30/04/2024, 15:30',
+      ciudad: 'Chiclana de la frontera, Cádiz',
+      descripcion: 'Celebra el Salón del Manga de Chiclana este fin de semana en Chiclana',
+      generos: [
+        {
+          id: 4,
+          denominacion: 'Cultura'
+        }
+      ]
+    }
+  ];
+
+  eventoSeleccionado: event = {
+      id: 0,
+      img: '',
+      nombre: '',
+      organizador: '',
+      fecha_hora: '',
+      ciudad: '',
+      descripcion: '',
+      generos: []
+  };
+
+  id: any;
+
+  constructor(private activatedRoute: ActivatedRoute){
+    console.log(this.activatedRoute);
+    this.activatedRoute.params.subscribe(params => {
+        this.id = params['id'] || null;
+        console.log(this.id-1);
+        this.eventoSeleccionado = this.eventos[this.id-1];
+      });
+  }
+
+  
+  
 }
